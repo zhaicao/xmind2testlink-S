7cb95e32b5930c0b88abe4fce59c74cf8eae0405
Ricky <ricky2971@hotmail.com>
2018-12-29 14:36:26 +0800

    删除构建内容

--- build/lib/xmind2testlink/__init__.py
+++
@@ -1,5 +0,0 @@
-config = {'logName': __name__,
-          'logLevel': None,
-          'logFormat': '%(asctime)s %(levelname)-8s: %(message)s',
-          'showTopicId': True,
-          'hideEmptyValue': False}
